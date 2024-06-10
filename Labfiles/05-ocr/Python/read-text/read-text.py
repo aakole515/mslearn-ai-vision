a81b46697153749ebb3e0fcfb79d1d1bb9d8084b
aakole515 <aakole515@gmail.com>
2024-06-10 09:38:25 -0700

    Labwork

--- Labfiles/05-ocr/Python/read-text/read-text.py
+++ Labfiles/05-ocr/Python/read-text/read-text.py
@@ -5,7 +5,9 @@ from PIL import Image, ImageDraw
 from matplotlib import pyplot as plt
 
 # Import namespaces
-
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 def main():
 
@@ -18,7 +20,10 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Authenticate Azure AI Vision client
-        
+        cv_client = ImageAnalysisClient(
+             endpoint=ai_endpoint,
+             credential=AzureKeyCredential(ai_key)
+        )
 
         # Menu for text reading functions
         print('\n1: Use Read API for image (Lincoln.jpg)\n2: Read handwriting (Note.jpg)\nAny other key to quit\n')
@@ -42,9 +47,44 @@ def GetTextRead(image_file):
             image_data = f.read()
 
     # Use Analyze image function to read text in image
+    result = cv_client.analyze(
+         image_data=image_data,
+         visual_features=[VisualFeatures.READ]
+    )
     
-    
+    if result.read is not None:
+         print("\n Text")
+         image = Image.open(image_file)
+         fig = plt.figure(figsize=(image.width/100, image.height/100))
+         plt.axis('off')
+         draw = ImageDraw.Draw(image)
+         color = 'cyan'
 
+         for line in result.read.blocks[0].lines:
+              #Return text detected in image
+              print(f' {line.text}')
+              drawLinePolygon = True
+              r = line.bounding_polygon
+              bounding_polygon = ((r[0].x, r[0].y),(r[1].x, r[1].y),(r[2].x, r[2].y))
+              # Draw line bounding polygon
+              if drawLinePolygon:
+                draw.polygon(bounding_polygon, outline=color, width=3)
+              # Return position of bounding box around each line
+                print("   Bounding Polygon: {}".format(bounding_polygon))
+              # Return each word detected in the image and the position 
+              # bounding box around each word with the confidence level 
+              # of each word
+              for word in line.words:
+                  r = word.bounding_polygon
+                  bounding_polygon = ((r[0].x, r[0].y), (r[1].x, r[1].y), (r[2].x, r[2].y))
+
+         plt.imshow(image)
+         plt.tight_layout(pad=0)
+         outputfile = 'text-lincon.jpg'
+         fig.savefig(outputfile)
+         print('\n Result saved in ', outputfile)
+
+        
 
 
 if __name__ == "__main__":
